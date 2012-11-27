9ac46a5b65f9664244d75c116d0a4fa5e3f093ba
Josh Wymer <wymetyme@gmail.com>
2012-11-26 23:08:03 -0800

    test

--- protected/config/main.php
+++ protected/config/main.php
@@ -58,8 +58,10 @@ return array(
 			'rules'=>array(
 				'<controller:\w+>/<id:\d+>'=>'<controller>/view',
 				'<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
+                '<controller:\w+>/<action:\w+>/<pass:\w+>'=>'<controller>/<action>',
 				'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
-			),
+
+            ),
 		),
 
 //		'db'=>array(
